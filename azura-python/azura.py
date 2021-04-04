9bd7b4a75f287fa652908c4cf053549a869238e3
Delta <77167689+Delta-Azura@users.noreply.github.com>
2021-04-04 17:46:56 +0200

    up

--- azura-python/azura.py
+++ azura-python/azura.py
@@ -4,20 +4,18 @@
 
 import os
 import time
-import cowsay
 
 def main():
   print("Hello world")
   os.system("sudo cards install cards.devel")
   os.system("sudo cards sync && sudo cards diff")
-  cowsay.cow("Avant de poursuivre veuillez vérifier que votre pkgfile se trouve bien dans l'emplacement prévu à cet effet")
+  print("Avant de poursuivre veuillez vérifier que votre pkgfile se trouve bien dans l'emplacement prévu à cet effet")
   print("Nous vous laissons vérifier que les conditions sont remplis")
   time.sleep(10.0)
-  name = input("Donnez nous le nom de votre paquet")
-  local =input("Donnez nous le chemin vers votre pkgfile")
-  os.system("cd " + local)
-  os.system("pkgmk -d")
-  os.system("sudo pkgadd " + name)
+  name=input("donnez nous le paquet de votre paquet : ")
+  os.chdir("/usr/ports/perso/" + name)
+  os.system("sudo pkgmk -d")
+  os.system("sudo pkgadd " + name + "1*")
   print("Done")
   print("Thanks to use azura, if you found a bug, please report it to us")
   
