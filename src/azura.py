c07c1e15d7f4d271820c55116d893d5e583164e3
Delta <77167689+Delta-Azura@users.noreply.github.com>
2021-04-06 15:46:52 +0200

    up

--- src/azura.py
+++ src/azura.py
@@ -3,14 +3,12 @@
 import os
 import sys  
 
-compile = sys.argv[1]
-install = sys.argv[1]
-purge = sys.argv[1]
-
+# Fonction principale, build le pkgfile
 def compile():
   print("Hello world")
   os.system("sudo cards install cards.devel && sudo cards sync && sudo cards upgrade")
   name =input("Donnez nous le nom de votre paquet : ")
+  # Vérification de la présence du chemin
   try:
       os.chdir("/usr/ports/perso/" + name)
   except:
@@ -22,7 +20,7 @@ def compile():
   print("Done")
   print("If you have found a bug, please report it")
 
-
+# Fonction install, permet d'installer un paquet, comming soon
 def install():
     print("Nous syncronisons les dépôts de votre os ... please wait ") 
     os.system("sudo cards sync")
@@ -33,6 +31,31 @@ def install():
     print("Done")
     exit()
 
+def update():
+    print("Nous mettons votre système à jour")
+    os.system("sudo cards sync")
+    print("Nous nettoyons les archives binaires")
+    os.system("sudo cards upgrade")
+    print("Done")
+
+# Comming soon
+def remove ():
+    rem =input("Quel paquet voulez vous supprimer : ")
+    os.system("sudo cards remove " + rem )
+    os.system("sudo cards purge")
+
+#  Les arguments
+if sys.argv[1] == "update":
+    update()
+
+if sys.argv[1] == "compile":
+    compile()
+
+if sys.argv[1] == "install":
+    install()
+
+if sys.argv[1] == "remove":
+    remove()
 
 
 # Les arguments
