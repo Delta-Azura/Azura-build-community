9c5bac535c20471a95e1cb40e1d8975ca33735ca
Delta <77167689+Delta-Azura@users.noreply.github.com>
2021-04-17 18:09:30 +0200

    import

--- base-install.py
+++ base-install.py
@@ -7,10 +7,15 @@ import os
 yes = "yes"
 
 def main():
-    print("Félicitations, vous venez d'installer la distribution GNU/LINUX NuTyX !! ")
-    print("Avant d'aller plus loin, veuillez svp lire le prérequi : Tout d'abord veuillez vérifier que votre ordinateur est connecté à internet")
+    print("Félicitations, vous venez d'installer la version BASE de la distribution GNU/LINUX NuTyX !! ")
+    print("Avant d'aller plus loin, veuillez svp lire le prérequi : Veuillez vérifier que votre ordinateur est connecté à internet")
     yes_or_no = input("Alors ? [yes ou no ] : ")
     if yes == yes_or_no :
+        print("Nous installons la BASE d'une installation graphique ... ")
+        os.system("sudo cards install lxdm xorg-xf86-video-intel zip unzip xdg-user-dirs sound-theme-freedesktop gvfs xorg-xf86-input-synaptics network-manager-applet")
+        firefox_install = input("Souhaitez vous installer un navigateur web ? [yes or no ] : ")
+        if firefox_install == yes:
+            os.system("sudo cards install firefox")
         print("Nous syncronisons les dépôts de votre os")
         os.system("sudo cards sync && sudo cards upgrade")
         install =input("Quels paquets souhaitez vous installer ? : ")
